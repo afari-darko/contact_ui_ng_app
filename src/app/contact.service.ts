4317a5e001d06734843f1545152300c889403e75
Festus <85620487+afari-darko@users.noreply.github.com>
2024-01-05 23:42:37 +0000

    Update contact.service.ts
    change the private URL in  the contact service class to the backend app URL

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://65.0.168.78:8081/"
+  private baseUrl="http:worker2:30200"
 
   constructor(private httpClient:HttpClient) { }
 
